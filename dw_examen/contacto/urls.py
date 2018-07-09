33550881112b3db05bdf49cd9c45d7b9a4d81023
jotaierreo <jairoandres.14@hotmail.com>
2018-07-08 21:56:47 -0400

    agregada llamadas al modelo en las vistas

--- dw_examen/contacto/urls.py
+++ dw_examen/contacto/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'contacto'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('contacto/<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
--- dw_examen/contacto/views.py
+++ dw_examen/contacto/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contacto
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    template_name = 'contacto/index.html'
+    model = Contacto
+    context_object_name = 'contactos'
+
+
+class DetailView(generic.DetailView):
+    model = Contacto
+    template_name = 'contacto/detail.html'
